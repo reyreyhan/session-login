d1927f9715219275028b41884dc044f9d150cd59
severalpuns <newrey9227@gmail.com>
2016-11-12 10:17:27 +0700

    0.1

---
+++ aa.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2016 at 04:14 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
+(1, 'Reyhan Alphard Savero', 'newrey9227@gmail.com', 'reyreyhan', 'c4ca4238a0b923820dcc509a6f75849b'),
+(2, 'ry', 'ry@gmail.com', 'ry', '4b43b0aee35624cd95b910189b3dc231'),
+(3, 'alpin', 'alpin@pjb.com', 'alpin', 'd9be8dacc34375b56ac0a93a46a014c8');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ application/views/v_crud.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="<?php echo base_url() ?>tampilan/wcpg/css/main.css"/>
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+
+	<body>
+
+		<!-- Banner -->
+			<section id="banner">
+				<h2><strong>Selamat Datang</strong> Login & Registrasi</h2>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper special">
+				<div class="inner">
+					<header class="major">
+						<h2>Pilih Menu di Bawah Ini</h2>
+					</header>
+                                        
+					<div class="features">
+                                         
+						<div class="feature">
+						
+						<a href ="index.php/c_tiket/lapor" style = "text-decoration:none">
+							<i class="fa fa-bug"></i>
+							<h3>Registrasi</h3>
+							<p>Silahkan Lapor Gangguan Anda Lewat Sini</p>
+						</a>
+						</div>
+
+						<div class="feature">
+						<a href ="index.php/c_tiket/lihat" style = "text-decoration:none">
+							<i class="fa fa-ticket"></i>
+							<h3>Login</h3>
+							<p>Lihat Apakah Laporan Anda Sudah Masuk</p>
+						</a>
+						</div>
+	
+					</div>
+				</div>
+
+                                                
+
+			</section>
+			
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+
+</html>
